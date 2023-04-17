0b92e670c98bfff15db8cf3ffa1e3e15e61b2e0f
Juan Manuel Perez <jm.perezs1@uniandes.edu.co>
2023-04-16 21:16:09 -0500

    taller 2

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import { series } from "./dataSeries.js";
 var seriesTbody = document.getElementById('series');
 var seriesTCaption = document.getElementById('promedioSeries');
+var infoTarjeta = document.getElementById('tarjeta');
 renderSeriesInTable(series);
 seriesTCaption.innerHTML = "Promedio de temporadas: ".concat(getPromedioTemporadas(series));
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                                <td style=\"color: dodgerblue;\">").concat(c.nombre, "</td>\n                                <td>").concat(c.canal, "</td>\n                                <td>").concat(c.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                                <td><button class=\"btn btn-link\">").concat(c.nombre, "</button></td>\n                                <td>").concat(c.canal, "</td>\n                                <td>").concat(c.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
+        var btn = trElement.querySelector(".btn");
+        btn.addEventListener('click', function () { return renderCard(c.nombre, c.imagen, c.descripcion, c.link); });
     });
 }
 function getPromedioTemporadas(series) {
     var suma = 0;
     series.forEach(function (c) { return suma = suma + c.temporadas; });
     return suma / series.length;
+}
+function renderCard(nombre, imagen, descripcion, link) {
+    infoTarjeta.style.visibility = 'visible';
+    infoTarjeta.innerHTML = "<img src=\"".concat(imagen, "\" class=\"card-img-top\" height=\"150\" width=\"200\" alt=\"").concat(nombre, " Image\">\n                              <div class=\"card-body\">\n                                <h5 class=\"card-title\">").concat(nombre, "</h5>\n                                <p class=\"card-text\">").concat(descripcion, "</p>\n                                <a href=").concat(link, " class=\"card-link\" target=\"_blank\">").concat(link, "</a>\n                              </div>");
 }
--- main.ts
+++ main.ts
@@ -1,22 +1,25 @@
 import { Serie } from "./serie.js";
-
 import { series } from "./dataSeries.js";
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 let seriesTCaption: HTMLElement = document.getElementById('promedioSeries')!;
+let infoTarjeta: HTMLElement = document.getElementById('tarjeta')!;
+
 
 renderSeriesInTable(series);
 
-seriesTCaption.innerHTML=`Promedio de temporadas: ${getPromedioTemporadas(series)}`
+seriesTCaption.innerHTML=`Promedio de temporadas: ${getPromedioTemporadas(series)}`;
 
 function renderSeriesInTable(series: Serie[]):void{
     series.forEach(c=>{
         let trElement = document.createElement("tr");
         trElement.innerHTML =  `<td>${c.id}</td>
-                                <td style="color: dodgerblue;">${c.nombre}</td>
+                                <td><button class="btn btn-link">${c.nombre}</button></td>
                                 <td>${c.canal}</td>
                                 <td>${c.temporadas}</td>`;
-        seriesTbody.appendChild(trElement);            
+        seriesTbody.appendChild(trElement);    
+        let btn = trElement.querySelector(".btn")!;
+        btn.addEventListener('click', ()=>renderCard(c.nombre, c.imagen, c.descripcion, c.link));
     });
 }
 
@@ -24,4 +27,14 @@ function getPromedioTemporadas(series: Serie[]):number{
     let suma: number = 0;
     series.forEach(c=>suma=suma+c.temporadas);
     return suma/series.length;
+}
+
+function renderCard(nombre:string, imagen:string, descripcion:string, link:string):void{
+    infoTarjeta.style.visibility = 'visible';
+    infoTarjeta.innerHTML = `<img src="${imagen}" class="card-img-top" height="150" width="200" alt="${nombre} Image">
+                              <div class="card-body">
+                                <h5 class="card-title">${nombre}</h5>
+                                <p class="card-text">${descripcion}</p>
+                                <a href=${link} class="card-link" target="_blank">${link}</a>
+                              </div>`;
 }
